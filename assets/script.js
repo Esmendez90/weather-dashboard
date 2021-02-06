2a8a56c48f70b3356f2cab12fa81cb1283f86c83
Esteban Mendez <edmendezn@gmail.com>
2021-02-05 23:13:14 -0500

    Add click event to render data of city from the list of searched cities

--- assets/script.js
+++ assets/script.js
@@ -7,10 +7,15 @@ var windSpeed = $("#wind-speed");
 var uvIndex = $("#uv-index");
 var weeklyTemp = $("#weekly-temp");
 var weeklyHumidity = $("#weekly-humidity");
+var ulTag = $("#saved-cities");
 
 // Displays weather data
+
 function displayCityWeather(city) {
+  $("#forecast").empty();
+
   var apiKey = "2f1c1bf2f40bf4e0bff9c4a2bf0f5904";
+
   // Current weather API
   var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
@@ -18,7 +23,6 @@ function displayCityWeather(city) {
     url: queryURL,
     method: "GET",
   }).then(function (response) {
-    console.log(response);
     // Output current date
     var date = new Date(response.dt * 1000);
     var forecastDate =
@@ -30,7 +34,6 @@ function displayCityWeather(city) {
     var weatherIconUrl = `https://openweathermap.org/img/wn/${iconData}@2x.png`;
     cityName.text(response.name);
     $("#weather-icon").attr("src", weatherIconUrl);
-    $("#weather-icon2").attr("src", weatherIconUrl);
 
     // Output Temperature
     cityTemp.text("Temperature: " + response.main.temp + " \xB0" + "F");
@@ -44,7 +47,6 @@ function displayCityWeather(city) {
     // Output UV index
     var latitude = response.coord.lat;
     var longitude = response.coord.lon;
-
     var oneCallURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
 
     // ajax request for 5 day forecast
@@ -52,64 +54,89 @@ function displayCityWeather(city) {
       url: oneCallURL,
       method: "GET",
     }).then(function (response) {
-      console.log(response);
-      uvIndex.text("UV Index: " + response.current.uvi);
+      // UV index color
+      var uvIndexValue = response.current.uvi;
 
-      // for loop that will get us the 5 day weather forecast of orlando
+      if (uvIndexValue >= 0 && uvIndexValue < 3) {
+        uvIndex.addClass("green");
+      } else if (uvIndexValue >= 3 && uvIndexValue < 6) {
+        uvIndex.addClass("yellow");
+      } else if (uvIndexValue >= 6 && uvIndexValue < 8) {
+        uvIndex.addClass("orange");
+      } else {
+        uvIndex.addClass("red");
+      }
 
+      // UV index value display on page
+      uvIndex.text(response.current.uvi);
+
+      // for loop that will get us the 5 day weather forecast
       for (var i = 1; i < 6; i++) {
         var date = new Date(response.daily[i].dt * 1000);
         var forecastDate =
           date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear();
+        var forecastIcon = response.daily[i].weather[0].icon;
+        var forecastIconUrl = `https://openweathermap.org/img/wn/${forecastIcon}@2x.png`;
         var forecastTemp = response.daily[i].temp.day;
         var forecastHumidity = response.daily[i].humidity;
 
-        // this will create 5 cards that will display the weather forecast for 5 days.
-        // for now, it is defaulted to ONLY show the 5 day forecast for orlando. See line 113 in js.
-
-        $("#forecast").append(`<div
-          id="forecast-card"
-          class="card text-white bg-primary mb-3"
-          style="max-width: 10rem"
-          >
-          <p class="card-text">${forecastDate}</p>
-          <p class="card-text">${forecastTemp}</p>
-          <p class="card-text">${forecastHumidity}</p>
-          </div>`);
+        // Create 5 cards that will display the weather forecast for 5 days.
+        $("#forecast").append(
+          `<div id="forecast-card" class="card text-white bg-primary mb-3" style="max-width: 10rem;"><p class="card-text">${forecastDate}</p><img src="${forecastIconUrl}"/><p class="card-text">${forecastTemp} \xB0F</p><p class="card-text">${forecastHumidity} %</p></div>`
+        );
       }
     });
   });
 }
 
+// When the user clicks on the search button then the user input will be stored in local storage
+// The city's data will be rendered on the page.
 searchBtn.on("click", function (event) {
   event.preventDefault();
-  //console.log(event.target);
 
   var citySearch = cityInput.val();
   var storage = JSON.parse(localStorage.getItem("city-weather"));
   storage.push(citySearch);
   localStorage.setItem("city-weather", JSON.stringify(storage));
+
   displayCityWeather(citySearch);
   renderCities();
 });
 
+// The previously searched cities will be appended to a ul list
+// Every new search will be added to the list.
 function renderCities() {
-  var ulTag = $("<ul>").addClass("list-group list-group-flush");
-  $("#list-of-cities").append(ulTag);
+  $("#saved-cities").empty();
 
   var storage = JSON.parse(localStorage.getItem("city-weather"));
+
   for (var i = 0; i < storage.length; i++) {
-    console.log(storage[i]);
-    var liTag = $("<li>").addClass("list-group-item saved-city").attr("id", i); // Need help here!!!!
+    //console.log(storage[i]);
+
+    var liTag = $("<li>");
+    liTag.addClass("list-group-item saved-city");
+    liTag.attr("id", i);
     ulTag.append(liTag);
     $("#" + i).text(storage[i]);
   }
 }
+
+
+// If the user clicks on a city from the list then the data of that city will be displayed
+ulTag.click(function (event) {
+  var element = event.target;
+  var index = element.id; // will give us the id of the li element the user clicks on
+  console.log(index); 
+  var megadeth = $("#" + index); // calling the id of the element and concatenating it with the index variable
+  var city = megadeth.text(); // will give us the text (the city name) of the targeted element
+  console.log(city);
+
+  displayCityWeather(city); // will run the function and give us the data of the city
+});
 
 if (localStorage.getItem("city-weather") === null) {
   localStorage.setItem("city-weather", JSON.stringify([]));
 }
 
 renderCities();
-
-displayCityWeather("orlando");
+displayCityWeather("New York"); // New york is set as a default city 
